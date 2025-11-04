5a1ef9b809fc16606b391c1920771f010669ef71
Raul Egas <raul.sobrinho@eaportal.org>
2025-11-03 21:05:17 -0300

    Criação do banco de dados

--- tabelas.sql
+++ tabelas.sql
@@ -18,3 +18,18 @@ cod_barra VARCHAR(50),
 FOREIGN KEY(cod_compra) REFERENCES cliente(cpf),
 FOREIGN KEY(cod_barra) REFERENCES produtos(cod_barra)
 )
+
+use api_crud;
+
+CREATE Table Cadastro_Programador(
+    id_programador INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR (100),
+    linguagem_de_Programacao VARCHAR (100),
+    tipo_de_programa VARCHAR (100),
+    idade INT
+);
+
+ALTER TABLE Cadastro_Programador AUTO_INCREMENT = 100;
+
+SELECT id_programador, nome, linguagem_de_Programacao, tipo_de_programa, idade FROM Cadastro_Programador;
+
